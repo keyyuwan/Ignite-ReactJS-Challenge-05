1597388f13489c02ad9ed513ce8cafcdd96457a6
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-05-31 21:34:38 -0300

    challenge going on

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,13 +30,73 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetravelling</title>
+      </Head>
+      <div className={styles.container}>
+        <header>
+          <img src="/Logo.svg" alt="logo" />
+        </header>
+        <main className={styles.postsContainer}>
+          {postsPagination.results.map(post => (
+            <div className={styles.post} key={post.uid}>
+              <Link href="#">
+                <a>{post.data.title}</a>
+              </Link>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <p>
+                  <img src="/calendar.svg" alt="calendar" />
+                  {post.first_publication_date}
+                </p>
+                <p>
+                  <img src="/user.svg" alt="user" />
+                  {post.data.author}
+                </p>
+              </div>
+            </div>
+          ))}
+        </main>
+      </div>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 20,
+    }
+  );
 
-//   // TODO
-// };
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results.map(post => ({
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM y',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    })),
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
